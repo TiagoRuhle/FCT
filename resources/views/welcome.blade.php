6ab9bca7fc8a388766f0218d1b481b6b3b847355
TiagoRuhle <50878143+TiagoRuhle@users.noreply.github.com>
2020-05-18 20:24:05 +0100

    view home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,56 @@
 @extends('layouts.myapp')
 @section('content')
-    <h1>very nice</h1>
+    @guest
+       <!--sem ter o log in feito-->
+       <div class="row">
+            <h1>Posts</h1>
+       </div>
+       <div class="row">
+          <div class="alert alert-warning" role="alert">
+            Caso queira criar um post terá de fazer <a href="{{ route('login') }}" class="alert-link">log-in</a>
+          </div>
+       </div>    
+    @else
+        <!--com o log in feito-->
+        <div class="row">
+            <div class="col-md-9">
+                <h1>Posts</h1>
+            </div>
+            <div class="col-md-3">
+                <a href="{{route('posts.create')}}" class="btn btn-block btn-primary">Novo Post</a>
+            </div>
+        </div>
+    @endguest
+    <!-- Tabela com posts sobre procura-->
+    <table class="table table-sm table-hover">
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Titulo</th>                                
+                <th scope="col">Area</th>
+                <th scope="col"></th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($posts as $post)
+                <tr>
+                    <td>{{$post->id}}</td>    
+                    <td>{{$post->titulo}}</td>                                        
+                    <td>
+                        @foreach ($post->area as $area)
+                            <span class="badge badge-success">{{$area->area}}</span>    
+                        @endforeach
+                    </td>
+                    <td class="text-right">
+                        <a href="{{route('posts.show',$post->id)}}" class="btn btn-sm btn-outline-secondary">+</a>
+                    </td>
+                </tr> 
+            @empty
+                <tr>
+                    <td colspan="4">Ainda não existem posts</td>
+                </tr>
+            @endforelse
+            
+        </tbody>
+    </table>
 @endsection

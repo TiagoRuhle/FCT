45fa8342c9aec34d3495414a48bd9d7c186ddf01
TiagoRuhle <50878143+TiagoRuhle@users.noreply.github.com>
2020-06-01 22:16:09 +0100

    mensagem de email automatica

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -1,2 +1,3 @@
 <!--Aqui vai estar escrito o mail que irá ser enviado para a empresa em caso de interesse do aluno-->
-<p>teste1</p>
+<p>Vi o seu post no site ae-salvaterra.pt/emprego e gostaria de me informar mais sobre a sua empresa.</p>
+<p>Email enviado automaticamente em nome de {{Auth::user()->name}}</p>
